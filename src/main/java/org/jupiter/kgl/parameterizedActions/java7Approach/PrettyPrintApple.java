0e94f0fd7a55237b5e3a0a46a771b8171258e7d6
kangguolong <kangguolong@gmail.com>
2018-12-01 01:52:08 +1300

    add project structure

--- src/main/java/org/jupiter/kgl/parameterizedActions/java7Approach/PrettyPrintApple.java
+++ src/main/java/org/jupiter/kgl/parameterizedActions/java7Approach/PrettyPrintApple.java
@@ -1,4 +1,14 @@
 package org.jupiter.kgl.parameterizedActions.java7Approach;
 
+import org.jupiter.kgl.parameterizedActions.Apple;
+
+import java.util.List;
+
 public class PrettyPrintApple {
+
+    public static void print(List<Apple> inventory, AppleFormatter formatter){
+        for (Apple apple : inventory) {
+            formatter.accept(apple);
+        }
+    }
 }
