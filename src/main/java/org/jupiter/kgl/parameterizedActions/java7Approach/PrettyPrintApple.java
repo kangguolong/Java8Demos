ad24fee014a0d7f919a8e24d335c9c7790ec1f06
kangguolong <kangguolong@gmail.com>
2018-12-01 01:58:08 +1300

    java 7 approach

--- src/main/java/org/jupiter/kgl/parameterizedActions/java7Approach/PrettyPrintApple.java
+++ src/main/java/org/jupiter/kgl/parameterizedActions/java7Approach/PrettyPrintApple.java
@@ -2,13 +2,20 @@ package org.jupiter.kgl.parameterizedActions.java7Approach;
 
 import org.jupiter.kgl.parameterizedActions.Apple;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PrettyPrintApple {
 
     public static void print(List<Apple> inventory, AppleFormatter formatter){
         for (Apple apple : inventory) {
-            formatter.accept(apple);
+            System.out.println(formatter.accept(apple));
         }
+    }
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(new Apple("green", 80), new Apple("red", 180));
+        print(inventory, new AppleSimpleFormatter());
+        print(inventory, new AppleFancyFormatter());
     }
 }
