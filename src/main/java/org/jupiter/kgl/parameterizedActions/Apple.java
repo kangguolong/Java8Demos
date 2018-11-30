7e14f43ed5609c265b258910cb286acc29be5ef1
kangguolong <kangguolong@gmail.com>
2018-12-01 01:33:06 +1300

    commit

---
+++ src/main/java/org/jupiter/kgl/parameterizedActions/Apple.java
@@ -0,0 +1,19 @@
+package org.jupiter.kgl.parameterizedActions;
+
+public class Apple {
+    private String colour;
+    private int weight;
+
+    public Apple(String colour, int weight){
+        this.colour = colour;
+        this.weight = weight;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
